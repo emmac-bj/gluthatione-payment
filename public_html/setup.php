917ad853737ef6577f19a1d8de9c73be7f325d14
emmac-bj <38691332+emmac-bj@users.noreply.github.com>
2018-04-24 17:40:04 +0200

    Update setup.php

--- public_html/setup.php
+++ public_html/setup.php
@@ -4,7 +4,7 @@ require_once '../lib/Braintree.php';
 
 // Instantiate a Braintree Gateway either like this:
 $gateway = new Braintree_Gateway([
-    'environment' => 'sandbox'
+    'environment' => 'sandbox',
     'merchantId' => 'fvky99ss46bby3jw',
     'publicKey' => 'm9psftrw4vtp4c3m',
     'privateKey' => '43dfe4f45110221cd9184cad88da75a9'
