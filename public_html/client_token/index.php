3c39a9b82cd43a7ee1a93222efa7b3617701e0f6
emmac-bj <38691332+emmac-bj@users.noreply.github.com>
2018-04-24 17:51:00 +0200

    Update index.php

--- public_html/client_token/index.php
+++ public_html/client_token/index.php
@@ -1,5 +1,11 @@
 <?php
-require_once('../setup.php');
+require_once ('../../lib/Braintree.php');
+$gateway = new Braintree_Gateway([
+    'environment' => 'sandbox',
+    'merchantId' => 'fvky99ss46bby3jw',
+    'publicKey' => 'm9psftrw4vtp4c3m',
+    'privateKey' => '43dfe4f45110221cd9184cad88da75a9'
+]);
 $aCustomerId = "Something";
 $clientToken = $gateway->clientToken()->generate([
     "customerId" => $aCustomerId
